7f47f481110f7dad16cc4d66ac96ccb71440affd
billvsme <994171686@qq.com>
2016-02-17 20:27:18 +0800

    Use gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const webpack = require('gulp-webpack');
+
+gulp.task('default', function() {
+    return  gulp.src('./src/js/app.jsx')
+    .pipe(webpack({
+        watch: true,
+        output: {
+            filename: 'bundle.js'
+        },
+        resolve: {
+            extensions: ['', '.js', '.jsx']
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.js|jsx$/,
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['es2015', 'react']
+                    }
+                }
+            ]
+        }
+    }))
+    .pipe(gulp.dest('./build'))
+});
